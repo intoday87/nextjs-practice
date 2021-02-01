74ca7d3608682f9e4826ffff35477dc3349eb1c3
USER <user@AD01423500.local>
2021-02-01 13:15:59 +0900

    Add next Image example
    모바일 기준 같은 640*640인데 next에서 최적화로 해상도를 조절해 20kb ->
    6kb

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ['shopping-phinf.pstatic.net']
+    }
+}
---
+++ pages/images/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+const Images: React.FC<{}> = ({}) => {
+    return (
+        <Image src="https://shopping-phinf.pstatic.net/main_2600057/2600057221.20140629200311.jpg?type=f640" layout="fill" />
+    )
+}
+
+export default Images
